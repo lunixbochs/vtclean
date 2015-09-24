49bb8b59924edbb0fe97a82959d45b52f812c210
Ryan Hileman <lunixbochs@gmail.com>
2015-09-24 06:07:55 -0700

    add line editing '@'

--- vtclean.go
+++ vtclean.go
@@ -8,7 +8,7 @@ import (
 // see regex.txt for a slightly separated version of this regex
 var vt100re = regexp.MustCompile(`^\033(([A-KZ=>12<]|Y\d{2})|\[\d+[A-D]|\[\d+;\d+[Hf]|#[1-68]|\[(\d+|;)*[qm]|\[[KJg]|\[[0-2]K|\[[02]J|\([ABCEHKQRYZ0-7=]|[\[K]\d+;\d+r|\[[03]g|\[\?[1-9][lh]|\[20[lh]|\[[56]n|\[0?c|\[2;[1248]y|\[!p|\[([01457]|254)}|\[\?(12;)?(25|50)[lh]|[78DEHM]|\[[ABCDHJKLMP]|\[4[hl]|\[\?1[46][hl]|\[\*[LMP]|\[[12][JK]|\]\d*;\d*[^\x07]+\x07|\[\d*[@ABCDEFGIJKLMPSTXZ1abcdeghilmnp])`)
 var vt100color = regexp.MustCompile(`^\033\[(\d+|;)*[m]`)
-var lineEdit = regexp.MustCompile(`^\033\[(\d*)([CDPK])`)
+var lineEdit = regexp.MustCompile(`^\033\[(\d*)([@CDPK])`)
 
 func vt100scan(line string) int {
 	return len(vt100re.FindString(line))
@@ -40,6 +40,11 @@ func Clean(line string, color bool) string {
 					n = 1
 				}
 				switch m[2] {
+				case "@":
+					for j := 0; j < n; j++ {
+						out[pos+j] = ' '
+					}
+					pos += n
 				case "C":
 					pos += n
 				case "D":
