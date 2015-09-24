f6e0fd6d691678c3cbce25e4495f1090f9416fab
Ryan Hileman <lunixbochs@gmail.com>
2015-09-24 05:53:00 -0700

    add better support for line editing commands

--- vtclean.go
+++ vtclean.go
@@ -2,11 +2,13 @@ package vtclean
 
 import (
 	"regexp"
+	"strconv"
 )
 
 // see regex.txt for a slightly separated version of this regex
 var vt100re = regexp.MustCompile(`^\033(([A-KZ=>12<]|Y\d{2})|\[\d+[A-D]|\[\d+;\d+[Hf]|#[1-68]|\[(\d+|;)*[qm]|\[[KJg]|\[[0-2]K|\[[02]J|\([ABCEHKQRYZ0-7=]|[\[K]\d+;\d+r|\[[03]g|\[\?[1-9][lh]|\[20[lh]|\[[56]n|\[0?c|\[2;[1248]y|\[!p|\[([01457]|254)}|\[\?(12;)?(25|50)[lh]|[78DEHM]|\[[ABCDHJKLMP]|\[4[hl]|\[\?1[46][hl]|\[\*[LMP]|\[[12][JK]|\]\d*;\d*[^\x07]+\x07|\[\d*[@ABCDEFGIJKLMPSTXZ1abcdeghilmnp])`)
 var vt100color = regexp.MustCompile(`^\033\[(\d+|;)*[m]`)
+var lineEdit = regexp.MustCompile(`^\033\[(\d*)([CDPK])`)
 
 func vt100scan(line string) int {
 	return len(vt100re.FindString(line))
@@ -17,17 +19,46 @@ func isColor(line string) bool {
 }
 
 func Clean(line string, color bool) string {
-	out := make([]rune, 0, len(line))
+	out := make([]rune, len(line))
 	liner := []rune(line)
 	hadColor := false
+	pos, max := 0, 0
 	for i := 0; i < len(liner); {
 		c := liner[i]
 		switch c {
 		case '\b':
-			if len(out) > 0 {
-				out = out[:len(out)-1]
-			}
+			pos -= 1
+		case '\x7f':
+			copy(out[pos:max], out[pos+1:max])
+			max -= 1
 		case '\033':
+			if m := lineEdit.FindStringSubmatch(string(liner[i:])); m != nil {
+				n, err := strconv.Atoi(m[1])
+				if err != nil || n > 10000 {
+					n = 1
+				}
+				switch m[2] {
+				case "C":
+					pos += n
+				case "D":
+					pos -= n
+				case "P":
+					most := max - pos
+					if n > most {
+						n = most
+					}
+					copy(out[pos:], out[pos+n:])
+					max -= n
+				case "K":
+					max = pos
+				}
+				if pos < 0 {
+					pos = 0
+				}
+				if pos > max {
+					pos = max
+				}
+			}
 			if !(color && isColor(string(liner[i:]))) {
 				skip := vt100scan(string(liner[i:]))
 				if skip > 0 {
@@ -36,15 +67,21 @@ func Clean(line string, color bool) string {
 				}
 			} else {
 				hadColor = true
-				out = append(out, c)
+				out[pos] = c
+				pos++
 			}
 		default:
 			if c == '\n' || c >= ' ' {
-				out = append(out, c)
+				out[pos] = c
+				pos++
 			}
+		}
+		if pos > max {
+			max = pos
 		}
 		i += 1
 	}
+	out = out[:max]
 	if hadColor {
 		out = append(out, []rune("\033[0m")...)
 	}
