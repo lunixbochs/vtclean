99af041082fef905a67c12a5c484d8da26ade604
Ryan Hileman <lunixbochs@gmail.com>
2015-12-22 22:59:33 -0800

    add io.Reader interface

--- vtclean/vtclean.go
+++ vtclean/vtclean.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
 	"github.com/lunixbochs/vtclean"
+	"io"
 	"os"
 )
 
@@ -12,8 +11,6 @@ func main() {
 	color := flag.Bool("color", false, "enable color")
 	flag.Parse()
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Println(vtclean.Clean(scanner.Text(), *color))
-	}
+	stdin := vtclean.NewReader(os.Stdin, *color)
+	io.Copy(os.Stdout, stdin)
 }
