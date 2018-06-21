2d01aacdc34a083dca635ba869909f5fc0cd4f41
Ryan Hileman <lunixbochs@gmail.com>
2018-06-21 16:23:53 -0700

    improve primary regex

--- vtclean_test.go
+++ vtclean_test.go
@@ -42,7 +42,7 @@ var tests = map[string]string{
 	"aaa \033[25;25mtest": "aaa test",
 
 	"bbb \033]4;1;rgb:38/54/71\033\\test": "bbb test",
-	"ccc \033]4;1;rgb:38/54/71test":       "ccc rgb:38/54/71test",
+	"ccc \033]4;1;rgb:38/54/71test":       "ccc gb:38/54/71test",
 
 	// tabs
 	"aa\tbb": "aa\tbb",
@@ -52,11 +52,7 @@ var tests = map[string]string{
 }
 
 var colorTests = map[string]string{
-	"aaa \033[25;25mtest": "aaa \033[25;25mtest\x1b[0m",
-
-	// [0;m cases
-	// TODO: trailing [0;m should probably not be overridden with [0m
-	// TODO: when [0;m seen, may not want to append [0m to the end
+	"aaa \033[25;25mtest":             "aaa \033[25;25mtest\x1b[0m",
 	"\033[0;m aa":                     "\033[0;m aa\033[0m",
 	"\033[32;1m$ echo foobar\033[0;m": "\033[32;1m$ echo foobar\033[0m",
 }
